866f4dfabc0f0cf79fedbf14ebf95cc1b0f76f18
SRAVS_VEERANKI <142927761+sravsveeranki@users.noreply.github.com>
2024-04-18 21:28:57 -0500

    Delete DogManagementSystem directory

--- DogManagementSystem/src/main/java/com/springBootProjects/DogManagementSystem/repository/TrainerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springBootProjects.DogManagementSystem.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.springBootProjects.DogManagementSystem.Models.Trainer;
-
-public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
-
-}
